5439e81961d90df67087fc62e67d6c9a09def9da
Frank Uwe <nerf@sms-digital.com>
2021-02-21 13:30:39 +0100

    now extracting import names from PosixPath given file names

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import List
 import re
 
 ROOT = '/Users/frankeschner/Documents/Projects/df-lib-python/datafactory/'
+TARGET = Path(ROOT).stem
 PATTERN = '(from\s+[A-Za-z0-9_]+(\.[A-Za-z0-9_]+)*\s+){0,1}import\s+(([A-Za-z0-9_]+)|(\((\s+[A-Za-z0-9_]+\,\s+)+\)))'
 COMPILER = re.compile(pattern=PATTERN, flags=re.IGNORECASE)
 
@@ -17,7 +18,25 @@ def get_files(root: str = ROOT, file_type: str = 'py') -> List[str]:
     return [file for file in Path(root).glob(f'**/*.{file_type}')]
 
 
-def get_imports(file: Path) -> List[str]:
+def get_import_name(file: Path) -> str:
+    """
+
+    :param file:
+    :return:
+    """
+    file = str(file.relative_to(ROOT))
+
+    if file.endswith('__init__.py'):
+        file = file[:-11]
+
+    name = TARGET + ''.join(['.' + f for f in file.split('/') if len(f) > 0])
+
+    if name.endswith('.py'):
+        return name[:-3]
+    return name
+
+
+def get_all_imports(file: Path) -> List[str]:
     """
 
     :param file:
@@ -28,11 +47,32 @@ def get_imports(file: Path) -> List[str]:
         lines = f.read()
     for match in re.finditer(pattern=PATTERN, string=lines):
         matches.append(match.group())
-    return matches, lines
+    return matches
+
+
+def remove_external_imports(imports: List[str]) -> List[str]:
+    """
+
+    :param imports:
+    :return:
+    """
+    return imports
+
+
+def get_imports(file: Path) -> List[str]:
+    """
+
+    :param file:
+    :return:
+    """
+    all_imports = get_all_imports(file=file)
+    return remove_external_imports(imports=all_imports)
 
 
 if __name__ == '__main__':
+    imports = {}
     files = get_files()
+
     for file in files:
-        m = get_imports(file=file)
-        a = 1
+        name = get_import_name(file=file)
+        imports[name] = get_imports(file=file)
