3c1ec66cb986be098e35a564ac3ff49e8203c1ac
Frank Uwe <nerf@sms-digital.com>
2021-03-03 15:01:07 +0100

    some major improvements and bugfixes.

--- imports.py
+++ imports.py
@@ -1,17 +1,16 @@
 from pathlib import Path
-from typing import List, Dict
+from typing import List, Dict, Set
 
 import re
 from constants import (
-    ROOT,
-    TARGET,
+    COMMENTS_PATTERN,
     GET_IMPORT_NAME_PATTERN,
     GET_ALL_IMPORTS_PATTERN,
     GET_LEVELS_PATTERN,
 )
 
 
-def get_files(root: str = ROOT, file_type: str = 'py') -> List[str]:
+def get_files(root: str, file_type: str = 'py') -> Set[str]:
     """
     get all files (also in sub-directories) from given root path with a specific file_type.
     Return them as a list of strings.
@@ -19,27 +18,48 @@ def get_files(root: str = ROOT, file_type: str = 'py') -> List[str]:
     :param file_type: type of files to collect
     :return: list of strings containing the path of each file
     """
-    return [file for file in Path(root).glob(f'**/*.{file_type}')]
+    return set([file for file in Path(root).glob(f'**/*.{file_type}') if '/venv/' not in file.name])
 
 
-def from_directory_to_import_name(file: Path) -> str:
+def from_directory_to_import_name(file: Path, root: str) -> str:
     """
     transform given directory file path to an import-like format.
     __init__.py files point to their parent directory.
     e.g. /my/super/fancy/__init__.py -> my.super.fancy
     :param file: path of file to transform
+    :param root: main directory to look for imports
     :return: import name of file
     """
-    file = str(file.relative_to(ROOT))
+    file = str(file.relative_to(root))
 
     if file.endswith('__init__.py'):
         file = file[:-11]
 
-    name = TARGET + ''.join(['.' + f for f in file.split('/') if len(f) > 0])
+    name = Path(root).stem + ''.join(['.' + f for f in file.split('/') if len(f) > 0])
 
     if name.endswith('.py'):
         return name[:-3]
     return name
+
+
+def read_file(file: Path) -> str:
+    """
+    read a given file and return its content
+    :param file: file to read as pathlib Path
+    :return: content of file as string
+    """
+    with file.open('r') as f:
+        lines = f.read()
+    return lines
+
+
+def remove_comments(lines: str) -> str:
+    """
+    removes all comments from a given string until the next line break.
+    :param lines: string to remove comments from
+    :return: string without comments
+    """
+    return re.subn(pattern=COMMENTS_PATTERN, repl=r'\n', string=lines)[0]
 
 
 def get_all_imports(file: Path) -> List[str]:
@@ -49,22 +69,23 @@ def get_all_imports(file: Path) -> List[str]:
     :return: list of strings. each string is an import statement within the given file.
     """
     matches = []
-    with file.open('r') as f:
-        lines = f.read()
+    lines = read_file(file=file)
+    lines = remove_comments(lines=lines)
     for match in re.finditer(pattern=GET_ALL_IMPORTS_PATTERN, string=lines):
         matches.append(match.group())
     return matches
 
 
-def remove_external_imports(imports: List[str]) -> List[str]:
+def remove_external_imports(imports: List[str], root: str) -> List[str]:
     """
     removes imports from external libraries.
-    this is achieved by checking if the given import name contains the TARGET directory
+    this is achieved by checking if the given import name contains the stem of the root directory
     (main directory of repo to analyze).
     :param imports: list of imports as strings.
+    :param root: main directory to look for imports
     :return: list of imports (as strings) without external libraries.
     """
-    return [match for match in imports if TARGET in match]
+    return [match for match in imports if Path(root).stem in match]
 
 
 def from_imports_to_import_names(imports: List[str]) -> List[str]:
@@ -78,14 +99,15 @@ def from_imports_to_import_names(imports: List[str]) -> List[str]:
     return [match.group(1) for match in (compiler.match(imp) for imp in imports) if match]
 
 
-def get_imports(file: Path) -> List[str]:
+def get_imports(file: Path, root: str) -> List[str]:
     """
     get all imports from a file, remove external libraries and get the import name.
     :param file: path to file to extract import names from.
+    :param root: main directory to look for imports
     :return: list of import names from a specific file.
     """
     all_imports = get_all_imports(file=file)
-    internal_imports = remove_external_imports(imports=all_imports)
+    internal_imports = remove_external_imports(imports=all_imports, root=root)
     return from_imports_to_import_names(imports=internal_imports)
 
 
--- main.py
+++ main.py
@@ -5,18 +5,25 @@ from imports import (
     get_levels,
 )
 
+import sys
+from tqdm import tqdm
 from graph import generate
 
-
 if __name__ == '__main__':
-    imports = {}
-    files = get_files()
+    if len(sys.argv) == 2:
+        root = sys.argv[1]
+    else:
+        print('Please ONLY include the directory path of the repository you want to generate a dependency graph for.')
+        sys.exit(1)
 
-    for file in files:
-        name = from_directory_to_import_name(file=file)
+    imports = {}
+    files = get_files(root=root)
+
+    for file in tqdm(files):
+        name = from_directory_to_import_name(file=file, root=root)
         imports[name] = {
             "levels": get_levels(name=name),
-            "targets": get_imports(file=file),
+            "targets": get_imports(file=file, root=root),
         }
 
-    generate(data=imports, level=2)
+    generate(data=imports, level=2, target='graph.pdf')
